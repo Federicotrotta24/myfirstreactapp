171b712ac48c7df27f048aca1f94cf906892d544
Federico Trotta <federico.rtrotta@gmail.com>
2021-04-19 15:22:32 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import NavBar from './Components/Navbar/navBar'
+import {FontAwesomeIcon} from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Manga store Bs
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <NavBar/>
+  )
+  
 }
 
 export default App;
---
+++ src/Components/Navbar/navBar.js
@@ -0,0 +1,45 @@
+import react from 'react';
+import logo from './logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+
+const NavBar = () =>{
+    return(
+        <header>
+            <nav>
+                <div className="nav-bar">
+                    <div className='logo-container'>
+                        <img className= 'logo' src={logo} alt="Logo"/>
+                        <div className="name-container">
+                            <h1 className="name">
+                            <span className='name-span'>8 BIT </span>  Comic Store  
+                            </h1>
+                        </div>  
+                    </div>
+                    <div className="carrito-container">
+                        <div>
+                            <a href=""> <FontAwesomeIcon className='logo-carrito' icon={faShoppingCart}/></a>
+                           
+                        </div>
+                        <div>
+                            <h3 className='separador'>|</h3>
+                        </div>
+                        <div>
+                            <h2 className='valor-total'>$ 0.00</h2>
+                        </div>
+                    </div>
+                    <div>
+                        <ul className='list-container'>
+                            <li className='list'><a href="" className='list-link'>Home</a></li>
+                            <li className='list'><a href="" className='list-link'>Productos</a></li>
+                            <li className='list'><a href="" className='list-link'>About</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
