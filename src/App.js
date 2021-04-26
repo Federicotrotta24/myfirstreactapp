6b958fc155bafd0cee610b63ae4861dc8c19521b
Federico Trotta <federico.rtrotta@gmail.com>
2021-04-26 18:03:15 -0300

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import NavBar from './Components/Navbar/navBar'
-import Body from './Components/Items/itemListContainer'
 import {FontAwesomeIcon} from '@fortawesome/free-solid-svg-icons'
 import './App.css';
-import itemListContainer from './Components/Items/itemListContainer';
+import ItemListContainer from './Components/Items/itemListContainer';
 import React from 'react';
 
 function App() {
-  const products = "aca estan mis productos"
 
   return(
     <React.Fragment>
       <NavBar/> 
-      <itemListContainer products= {products}/>
-      <Body/>
+      <ItemListContainer saludo='Bienvenidos'/>
+      
     </React.Fragment>
   )
 }
--- src/Components/Items/itemListContainer.js
+++ src/Components/Items/itemListContainer.js
@@ -1,66 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import Foto from "./fotosProductos/watchmen.png";
 
-const itemListContainer = (Props) => {
-const Products = "aca estan mis productos"
+const ItemListContainer = (Props) => {
+  const [contador, setContador] = useState(1);
+
+  function agregarCarrito() {
+    setContador(contador + 1);
+    alert("Usted tiene " + contador + " productos en el carrito");
+  }
 
   return (
     <body>
+      <h2 className="saludo"> {Props.saludo} </h2>
       <div className="card-container">
         <div className="card">
           <div className="card-photo-container">
             <img className="card-photo" src={Foto} alt="Foto" />
           </div>
-          <div className="card-name">Watchmen</div>
-          <div className="card-description">
-            A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
-            WATCHMEN, cambiando el curso de la historia de los cómics.
+          <div className="description-container"> 
+            <div className="card-name">Watchmen</div>
+            <div className="card-description">
+              A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
+              WATCHMEN, cambiando el curso de la historia de los cómics.
+            </div>
+            <div className="price">$4000</div>
           </div>
-          <div className="price">$4000</div>
-          <div className="card-btn-compra">
-            <button>Agregar al carrito</button>
+          <div>
+            <button className="card-btn-compra" onClick={agregarCarrito}>
+              Comprar!
+            </button>
           </div>
         </div>
+
         <div className="card">
           <div className="card-photo-container">
             <img className="card-photo" src={Foto} alt="Foto" />
           </div>
-          <div className="card-name">Watchmen</div>
-          <div className="card-description">
-            A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
-            WATCHMEN, cambiando el curso de la historia de los cómics.
+          <div className="description-container">
+            <div className="card-name">Watchmen</div>
+            <div className="card-description">
+              A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
+              WATCHMEN, cambiando el curso de la historia de los cómics.
+            </div>
+            <div className="price">$4000</div>
           </div>
-          <div className="price">$4000</div>
-          <div className="card-btn-compra">
-            <button>Agregar al carrito</button>
+          <div>
+            <button className="card-btn-compra" onClick={agregarCarrito}>
+              Comprar!
+            </button>
           </div>
         </div>
+
         <div className="card">
           <div className="card-photo-container">
             <img className="card-photo" src={Foto} alt="Foto" />
           </div>
-          <div className="card-name">Watchmen</div>
-          <div className="card-description">
-            A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
-            WATCHMEN, cambiando el curso de la historia de los cómics.
+          <div className="description-container">
+            <div className="card-name">Watchmen</div>
+            <div className="card-description">
+              A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
+              WATCHMEN, cambiando el curso de la historia de los cómics.
+            </div>
+            <div className="price">$4000</div>
           </div>
-          <div className="price">$4000</div>
-          <div className="card-btn-compra">
-            <button>Agregar al carrito</button>
+          <div>
+            <button className="card-btn-compra" onClick={agregarCarrito}>
+              Comprar!
+            </button>
           </div>
         </div>
+
         <div className="card">
           <div className="card-photo-container">
             <img className="card-photo" src={Foto} alt="Foto" />
           </div>
-          <div className="card-name">Watchmen</div>
-          <div className="card-description">
-            A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
-            WATCHMEN, cambiando el curso de la historia de los cómics.
+          <div className="description-container">
+            <div className="card-name">Watchmen</div>
+            <div className="card-description">
+              A mediados de los ochenta, Alan Moore y Dave Gibbons crearon
+              WATCHMEN, cambiando el curso de la historia de los cómics.
+            </div>
+            <div className="price">$4000</div>
           </div>
-          <div className="price">$4000</div>
-          <div className="card-btn-compra">
-            <button>Agregar al carrito</button>
+          <div>
+            <button className="card-btn-compra" onClick={agregarCarrito}>
+              Comprar!
+            </button>
           </div>
         </div>
       </div>
@@ -68,4 +93,4 @@ const Products = "aca estan mis productos"
   );
 };
 
-export default itemListContainer;
+export default ItemListContainer;
